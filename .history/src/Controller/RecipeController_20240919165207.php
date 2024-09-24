3d6ff635b7806278b532e1850738a625156d7aa8
Eloi Bastide <eloi.bastide92@gmail.com>
2024-09-24 09:39:02 +0200

    dossier symfony

---
+++ .history/src/Controller/RecipeController_20240919165207.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return new Response ('recettes');
+
+    }
+
+    #[Route('/recette', name: 'recipe.show', requirements: ['id' =>  '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return new Response ('recettes');
+
+    }
+}
---
+++ .history/src/Controller/RecipeController_20240920122204.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository): Response
+    {
+        $recipes = $repository -> findWithDurationLowerThan(100);
+        return $this -> render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
+    }
+
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' =>  '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
+    {
+        $recipe = $repository -> find($id);
+        if ($recipe -> getSlug() != $slug){
+            return $this -> redirectToRoute('recipe.show', ['slug' => $recipe -> getSlug(), 'id' => $recipe -> getId()]);
+        }
+        return $this -> render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id
+        ]);
+    }
+}
